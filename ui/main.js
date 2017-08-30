22f5012317f48e8f80d976731f35ca662dd7bd34
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-08-30 23:14:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var img=document.getElementById('hasura');
+var marginL=0;
+function moveRight()
+{
+    marginL=marginL+5;
+    img.style.marginLeft=marginL+'px';
+    
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+};
