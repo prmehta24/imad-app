0f9c47fafaeffb318a30ce49a110aae26301cc80
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-08-31 22:25:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,21 @@
 
-var button = document.getElementById('counter');
-var counter=0;
-button.onclick = function()
-{
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-};
+ console.log('Loaded!');
+ var img=document.getElementById('hasura');
+ var marginL=0;
+ function moveRight()
+ {
+     marginL=marginL+1;
+     img.style.marginLeft=marginL+'px';
+     
+  }
+  img.onclick=function(){
+      var interval=setInterval(moveRight,100);
+ };
+ var button = document.getElementById('counter');
+ var counter=0;
+ button.onclick = function()
+ {
+     counter+=1;
+     var span=document.getElementById('count');
+     span.innerHTML=counter.toString();
+ };
