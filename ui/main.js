47798293f8a2af5fe98ada8466422bf7f3fdc83e
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-08-31 22:18:03 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,9 @@
+
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function()
+{
+    counter+=1;
+    var span=document.getElementById('count');
+    span.InnerHTML=counter.toString();
+};
