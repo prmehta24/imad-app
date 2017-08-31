a5b0d1949baf4fb37c5fe9a9f470ebfe027d1a81
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-08-31 22:36:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,10 +12,23 @@
       var interval=setInterval(moveRight,100);
  };
  var button = document.getElementById('counter');
- var counter=0;
+ 
  button.onclick = function()
  {
-     counter+=1;
-     var span=document.getElementById('count');
-     span.innerHTML=counter.toString();
+     var request = new XMLHttpRequest();
+     request.onreadystatechange=function()
+     {
+     if(request.readystate===XMLHttpRequest.DONE)
+     {
+     if(request.status===200)
+     {
+         var counter=request.responseText();
+      var span=document.getElementById('count');
+     span.innerHTML=counter.toString();   
+     }
+     }
+     };
+     request.open('GET','http://prmehta24.imad.hasura-app.io/counter',true);
+     request.send(null);
+     
  };
