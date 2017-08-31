b3de0b55f73c783620690eadc564b99075528d31
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-08-31 23:44:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,14 +35,30 @@
  var nameInput=document.getElementById('name');
  var name=nameInput.value;
  var submit=document.getElementById('submitbtn');
- submit.onclick=function()
- {
-   var names=['com 1','com 2','com 3'];  
-   var list='';
-   for(var i=0;i<names.length;i++)
-   {
-       list+='<li>'+names[i]+'</li>'
-   }
-   var ul=document.getElementById('namelist');
-   ul.innerHTML=list;
- };
+ submit.onclick=function(){
+  var request = new XMLHttpRequest();
+     request.onreadystatechange=function()
+     {
+     if(request.readyState===XMLHttpRequest.DONE)
+     {
+     if(request.status===200)
+     {
+         var names=request.responseText;
+         names=JSON.parse('names');
+         var list='';
+          
+     
+    for(var i=0;i<names.length;i++)
+     {
+ 
+        list+='<li>'+names[i]+'</li>';
+     }
+ 
+    var ul=document.getElementById('namelist');
+     ul.innerHTML=list;
+         
+     }
+     }
+     };};
+      request.open('GET','http://prmehta24.imad.hasura-app.io/submit-name?name='+name,true);
+     request.send(null);
