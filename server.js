ce5d957e65876969d9aae8323040074df4acdb56
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-08-31 14:37:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+var counter=0;
+  app.get('/counter', function (req, res) {
+      counter+=1;
+  res.send(counter.toString());
+});
   app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
  
