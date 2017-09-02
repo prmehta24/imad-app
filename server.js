0080f01f4a11cb7d7787cb6a953c51a0ea7b4551
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-09-03 00:28:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/submit-name', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
     
-    pool.query("SELECT * FROM article WHERE title= '" +req.params.articleName+ "'", function(err,result)
+    pool.query("SELECT * FROM article WHERE title=$1 ", [req.params.articleName], function(err,result)
     {
        
         if(err)
