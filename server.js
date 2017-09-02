22df5ce339f57ca20f481a72ebafdb2897a47e5d
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-09-02 23:40:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/submit-name', function (req, res) {
   res.send(JSON.stringify(names));
 });
 
-app.get('/article/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     
     pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName+"'", function(err,result)
     {
