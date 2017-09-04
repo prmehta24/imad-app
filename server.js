e05bfc9db0be890dc5d5d5f721a0a53d4e8763fb
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-09-04 21:05:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
 function hash(input,salt)
 {
   var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-  res.send(hashed);
+  //res.send(hashed);
   return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res)
