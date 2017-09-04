3d7a082799355e65730847eea37dd85ac683440c
prmehta24 <31245318+prmehta24@users.noreply.github.com>
2017-09-04 20:48:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
   user: 'prmehta24',
   database:'prmehta24',
@@ -33,6 +34,16 @@ app.get('/test-db', function (req, res) {
     })
  
 });
+function hash(input,salt)
+{
+  var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+  return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res)
+{
+    var hashedString=(req.params.input,'random-string');
+    res.send(hashedString);
+})
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
